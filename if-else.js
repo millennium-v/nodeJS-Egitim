6fa84fb663ed789dccf04ebd7627d612c7980412
Semih Çelik <semihcelik@stickercode.com>
2021-05-02 15:54:47 +0300

    Koşullandırma, Allahın Selamını Almayan Adamı Öldürme Oyunu

---
+++ if-else.js
@@ -0,0 +1,38 @@
+/*Koşullandırma,
+Eğer = if
+ */
+/*
+var isterMisin =true;
+
+if (isterMisin){
+    console.log("al bunu");
+}*/
+
+var isterMisin =false;
+var iyiMisin = false;
+
+if (isterMisin){
+    console.log("al bunu");
+}else if (!iyiMisin){
+    console.log("beter ol")
+
+}else {
+    console.log("malsın");
+}
+
+/* Allahın selamını almayan adamı öldürme oyunu */
+
+var selamunAleyküm =false;
+var geber = false;
+
+if (selamunAleyküm){
+    console.log("AleykümSelam")
+}else
+    if (geber){
+        console.log("Öldür!")
+    }
+    else{
+        console.log("Boş Koy!")
+    }
+
+
