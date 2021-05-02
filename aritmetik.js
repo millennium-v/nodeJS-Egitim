6f09da0c8a0db820ad8a996fe0a73434b1bff3d0
Semih Çelik <semihcelik@stickercode.com>
2021-05-02 15:04:18 +0300

    Aritmetik Operatörler

--- aritmetik.js
+++ aritmetik.js
@@ -7,8 +7,22 @@ String- Yazılar vs. İnteger - Tam Sayı
 * ** İnteger **
 * var sayi = 2;
 console.log(sayi + 3);
-*
+* Çarpma,bölme * /
+* Kalan yapımı %
 *
  */
 var sayi = 2;
-console.log(sayi + "3");
+console.log(sayi % 2);
+
+var sayi = 10;
+console.log(++sayi);
+console.log(sayi);
+
+var sayi = 10;
+var yeniSayi = (sayi -= 2); // sonucu 8 verir //
+console.log(yeniSayi);
+
+var sayi = 10;
+var yeniSayi = (sayi *= 9); // sonucu 90 verir //
+console.log(yeniSayi);
+
