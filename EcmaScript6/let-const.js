1eed6ed331b28951bad4854bd4a7543639a8b4dc
Semih Çelik <semihcelik@stickercode.com>
2021-05-04 15:19:15 +0300

    Let Const, var kullanmayı bırakmak

---
+++ EcmaScript6/let-const.js
@@ -0,0 +1,33 @@
+/*
++ var, let, const arasındaki farklar şunlardır :
+yok :D
+const sabit demektir
+ */
+
+/*
+for (let i = 0; i < 10; i++){
+    //
+}
+
+console.log(i);
+*/
+
+/*
+let x = 'foo';
+x = 'bar';
+
+console.log(x);
+*/
+
+let pi = 3;
+pi = 3.14;
+const degerler = [2,3,4,5];
+degerler.push(3);
+
+
+console.log(degerler);
+
+const isimler = {name: "semih", surname: "çelik"  };
+
+
+console.log(isimler);
