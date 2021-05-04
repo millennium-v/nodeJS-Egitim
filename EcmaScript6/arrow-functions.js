625936eb866d92fd124337f8d602f862c3d06ed9
Semih Çelik <semihcelik@stickercode.com>
2021-05-04 17:22:23 +0300

    Arrow Functions

---
+++ EcmaScript6/arrow-functions.js
@@ -0,0 +1,14 @@
+/* Eskiden
+
+var fonksiyonum = function () {
+    return 1;
+};
+
+console.log(fonksiyonum());
+
+*/
+
+const fonksiyonum = (sayi1, sayi2) => sayi1 + sayi2;
+;
+
+console.log(fonksiyonum(12, 8));
